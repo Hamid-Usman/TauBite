fc0a69267c26edf84baec1df9da86dd6e0cb43ac
Hamid-Usman <abdulhamidusman218@gmail.com>
2025-04-19 02:45:23 +0100

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}", 
+    // Add other directories if needed, e.g.:
+    // "./components/**/*.{js,ts,jsx,tsx}",
+    // "./pages/**/*.{js,ts,jsx,tsx}",
+  ],
+}
